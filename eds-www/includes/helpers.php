5dc59dc212700f6db40f64fcc7f5b34cee1e1097
canofnoodles <32342000+canofnoodles@users.noreply.github.com>
2017-11-12 15:23:01 -0500

    Update helpers.php

--- eds-www/includes/helpers.php
+++ eds-www/includes/helpers.php
@@ -1,8 +1,9 @@
 <?php
 $debug = true;
 # Author: Jonathan Smith, Paul Ippolito, David Cyganowski
-# Shows the records in prints
-function show_records($dbc) {
+# Shows the records in the presidents table
+function show_records($dbc)
+{
 	# Create a query to get the name and price sorted by price
 	$query = 'SELECT num, fname, lname FROM presidents ORDER BY num DESC' ;
 	# Execute the query
@@ -17,17 +18,18 @@ function show_records($dbc) {
   		echo '<TABLE>';
   		echo '<TR>';
   		echo '<TH>Number</TH>';
-        echo '<TH>First Name</TH>';
+		echo '<TH>First Name</TH>';
   		echo '<TH>Last Name</TH>';
   		echo '</TR>';
+		
   		# For each row result, generate a table row
   		while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
   		{
-    		echo '<TR>' ;
-    		echo '<TD>' . $row['num'] . '</TD>' ;
-    		echo '<TD>' . $row['fname'] . '</TD>' ;
-            echo '<TD>' . $row['lname'] . '</TD>' ;
-    		echo '</TR>' ;
+			echo '<TR>' ;
+			echo '<TD>' . $row['num'] . '</TD>' ;
+			echo '<TD>' . $row['fname'] . '</TD>' ;
+			echo '<TD>' . $row['lname'] . '</TD>' ;
+			echo '</TR>' ;
   		}
   		# End the table
   		echo '</TABLE>';
@@ -35,49 +37,116 @@ function show_records($dbc) {
   		mysqli_free_result( $results ) ;
 	}
 }
-# Inserts a record into the prints table
-function insert_record($dbc, $num, $fname, $lname) {
-  $query = 'INSERT INTO presidents(num, fname, lname, dob) VALUES ("' . $num . '", "' . $fname . '", "' . $lname . '", "' . '2001-11-16 00:00:00")' ;
-  show_query($query);
-  $results = mysqli_query($dbc,$query) ;
-  check_results($results) ;
-  return $results ;
+function show_link_records($dbc)
+{
+	# Create a query to get the name and price sorted by price
+	$query = 'SELECT num, lname FROM presidents ORDER BY num DESC' ;
+	# Execute the query
+	$results = mysqli_query( $dbc , $query );
+	check_results($results);
+	# Show results
+	if( $results )
+	{
+  		# But...wait until we know the query succeed before
+  		# rendering the table start.
+  		echo '<H1>Presidents</H1>';
+  		echo '<TABLE>';
+  		echo '<TR>';
+  		echo '<TH>Number</TH>';
+  		echo '<TH>Last Name</TH>';
+  		echo '</TR>';
+		
+  		# For each row result, generate a table row
+  		while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+  		{
+			echo '<TR>';
+			$alink = '<A HREF=linkypresidents.php?num=' . $row['num']  . '>' . $row['num'] . '</A>';
+			echo '<TD>' . $alink . '</TD>';
+			echo '<TD>' . $row['lname'] . '</TD>';
+			echo '</TR>';
+  		}
+  		# End the table
+  		echo '</TABLE>';
+  		# Free up the results in memory
+  		mysqli_free_result( $results );
+	}
+}
+function show_record($dbc, $num)
+{
+	# Create a query to get the name and price sorted by price
+	$query = 'SELECT num, fname, lname FROM presidents WHERE num = ' . $num;
+	# Execute the query
+	$results = mysqli_query( $dbc , $query );
+	check_results($results);
+	# Show results
+	if( $results )
+	{
+  		# But...wait until we know the query succeed before
+  		# rendering the table start.
+  		echo '<H1>Presidents</H1>';
+  		echo '<TABLE>';
+  		echo '<TR>';
+  		echo '<TH>Number</TH>';
+		echo '<TH>First Name</TH>';
+  		echo '<TH>Last Name</TH>';
+  		echo '</TR>';
+		
+  		# For each row result, generate a table row
+  		while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+  		{
+			echo '<TR>' ;
+			echo '<TD>' . $row['num'] . '</TD>' ;
+			echo '<TD>' . $row['fname'] . '</TD>' ;
+			echo '<TD>' . $row['lname'] . '</TD>' ;
+			echo '</TR>' ;
+  		}
+  		# End the table
+  		echo '</TABLE>';
+  		# Free up the results in memory
+  		mysqli_free_result( $results ) ;
+	}
+}
+# Inserts a record into the presidents table
+function insert_record($dbc, $num, $fname, $lname)
+{
+	$query = 'INSERT INTO presidents(num, fname, lname, dob) VALUES ("' . $num . '", "' . $fname . '", "' . $lname . '", "' . '2001-11-16 00:00:00")' ;
+	show_query($query);
+	$results = mysqli_query($dbc,$query) ;
+	check_results($results) ;
+	return $results ;
 }
 # Shows the query as a debugging aid
-function show_query($query) {
-  global $debug;
-  if($debug)
-    echo "<p>Query = $query</p>" ;
+function show_query($query)
+{
+	global $debug;
+	if($debug)
+		echo "<p>Query = $query</p>" ;
 }
 # Checks the query results as a debugging aid
-function check_results($results) {
-  global $dbc;
-  if($results != true)
-    echo '<p>SQL ERROR = ' . mysqli_error( $dbc ) . '</p>'  ;
+function check_results($results)
+{
+	global $dbc;
+	if($results != true) 
+		echo '<p>SQL ERROR = ' . mysqli_error( $dbc ) . '</p>'  ;
 }
-
-
-function valid_name ($name) {
-    if(empty($name))
-       return false;
-    return true;
+# Validates a given name
+function valid_name ($name)
+{
+	if(empty($name))
+		return false;
+	return true;
 }
-
-
-
-function valid_number ($num) {
-    if(empty($num) || !is_numeric($num))
-        return false;
-    else {
-        $num = intval ($num);
-    if($num <= 0)
-        return false;
-    }
-    return true;
-    
-
+# Validates a given number
+function valid_number ($num)
+{
+	if(empty($num) || !is_numeric($num))
+		return false;
+	else
+	{
+		$num = intval ($num);
+		if($num <= 0)
+			return false;
+	}
+	return true;
 }
-
-
-
 ?>
